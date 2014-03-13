b582d70d970b197b42510f670b80f013da6ddf27
akrish44 <akrish44@aol.com>
2014-03-12 19:18:56 -0600

    Finished Selection.java

--- Selection.java
+++ Selection.java
@@ -1,40 +1,23 @@
 public class Selection {
-	
-	public int[] numbers = {3, 1, 6, 7, 1, 2};
-	public static void main(String[] args) {
-		
-	}
-	public Selection(int[] numbers) {
-		int placeHolder = numbers[0];
-		i
-
-		for (int k=1; k<numbers.length(); k++) {
-			for (int s = placeHolder + 1; s<numbers.length(); s++) {
-				if (numbers[s] < numbers[k-1]) {
-					;
-					
-				}
-			}
-			
-		}
-	}
-
-	public void print(int[] numbers) {
-		for (int i=0; i<numbers.length(); i++) {
-			System.out.println(numbers[i]);
-		}
-	}
-
-	public boolean isSorted(int[] numbers) {
-		for (int j=1; j<numbers.length; j++) {
-			if (numbers[j] <= numbers[j-1]) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-
-	
-	}
+   
+    public static void main(String[] args) {
+        int arr[]={3,1,6,7,1,2};
+        int lowest, i, j;
+        for(i = 0 ; i < arr.length-1; i++) {  
+            lowest = i;                       
+            for(j = i+1 ; j < arr.length; j++) { 
+                if(arr[j] < arr[lowest]) {
+                    lowest = j;                  
+                }
+            }
+            if(lowest != i) {                   
+                int temp = arr[i];
+                arr[i] = arr[lowest];
+                arr[lowest] = temp;
+            }
+        }
+        for (int k = 0; k <= arr.length-1 ; k++) {
+        System.out.println(arr[k] + " ");
+        }
+    }
 }
