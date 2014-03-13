ce48dfd0ce0ca88fb0df9a60e9e06b590ab84974
akrish44 <akrish44@aol.com>
2014-03-13 15:25:02 -0600

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -2,10 +2,11 @@ public class Selection {
    
     public static void main(String[] args) {
         int arr[]={3,1,6,7,1,2};
-        int lowest, i, j;
-        for(i = 0 ; i < arr.length-1; i++) {  
+        int lowest=0;
+
+        for(int i = 0 ; i < arr.length-1; i++) {  
             lowest = i;                       
-            for(j = i+1 ; j < arr.length; j++) { 
+            for(int j = i+1 ; j < arr.length; j++) { 
                 if(arr[j] < arr[lowest]) {
                     lowest = j;                  
                 }
