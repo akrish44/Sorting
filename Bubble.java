b16d5c06124425be39d7f7389fddbd3ca996927c
akrish44 <akrish44@aol.com>
2014-03-14 08:49:26 -0600

    Update Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -2,39 +2,40 @@ import java.util.Arrays;
 public class Bubble {
   public static void main(String[] args) {
         int[] arr = {3, 1, 6, 7, 1, 2};
+
         System.out.println(Arrays.toString(arr));
         swap(arr); 
     }
 
     private static void swap(int[] arr) {
     int temp = 0;
-    for (int i = 0; i < arr.length; i++) {
-        for (int j = 1; j < (arr.length - i); j++) {
-            if (arr[j - 1] > arr[j]) {
-                temp = arr[j - 1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-            }
+    boolean done = false;
+    while (!done) {
+        // for (int j = 1; j < (arr.length); j++) {
+        //     if (arr[j - 1] > arr[j]) {
+        //         temp = arr[j - 1];
+        //         arr[j - 1] = arr[j];
+        //         arr[j] = temp;
+        //     }
+            
+           
+        // }
+        done = true;
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i]>arr[i+1]) {
+                done = false;
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
 
-        }
-        if (isSorted(arr)) {
-            break;
-        } else {
+            }
             print(arr);
         }
-        
+       
 
         }
     }
 
-    public static boolean isSorted(int[] arr) {
-        for (int i=0; i<arr.length-1; i++) {
-            if (!(arr[i]<arr[i+1])) {
-               return false;
-            }
-        }
-        return true;
-    }
 
 
     private static void print(int[] arr) {
