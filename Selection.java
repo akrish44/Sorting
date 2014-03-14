dc1a43f2206a65a0bf229d460814f47bc775bd56
akrish44 <akrish44@aol.com>
2014-03-13 21:27:31 -0600

    Finished Selection.java

--- Selection.java
+++ Selection.java
@@ -1,24 +1,49 @@
+import java.util.Arrays;
 public class Selection {
-   
-    public static void main(String[] args) {
-        int arr[]={3,1,6,7,1,2};
-        int lowest=0;
 
-        for(int i = 0 ; i < arr.length-1; i++) {  
-            lowest = i;                       
-            for(int j = i+1 ; j < arr.length; j++) { 
-                if(arr[j] < arr[lowest]) {
-                    lowest = j;                  
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 6, 7, 1 ,2};
+        System.out.println(Arrays.toString(arr));
+        selectionSort(arr);
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
             }
-            if(lowest != i) {                   
-                int temp = arr[i];
-                arr[i] = arr[lowest];
-                arr[lowest] = temp;
+            swap(arr, i, min);
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        print(arr);
+
+    }
+
+    
+    public boolean isSorted(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (!(arr[i]<arr[i+1])) {
+               return false;
             }
         }
-        for (int k = 0; k <= arr.length-1 ; k++) {
-        System.out.println(arr[k] + " ");
-        }
+        return true;
     }
+
+    public static void print(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println("\n");
+
+    }
+
 }
