42ffccb75b27a67bf5cfb619e354d092bb84e9fd
akrish44 <akrish44@aol.com>
2014-03-13 21:36:55 -0600

    Finished Bubble.java

--- Bubble.java
+++ Bubble.java
@@ -1,27 +1,30 @@
+import java.util.Arrays;
 public class Bubble {
   public static void main(String[] args) {
-        int[] arr = {3, 1, 6, 7, 1, 2}; 
-
-        bubbleSort(arr, arr.length); 
-
-        
-
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        int[] arr = {3, 1, 6, 7, 1, 2};
+        System.out.println(Arrays.toString(arr));
+        swap(arr); 
     }
 
-    private static void bubbleSort(int[] arr, int length) {
-
-        for(int counter=0; counter<length-1; counter++) { 
-            for(int index=0; index<length-1-counter; index++) { 
-                if(arr[index] > arr[index+1]) { 
-                    int temp = arr[index]; 
-                    arr[index] = arr[index+1];
-                    arr[index+1] = temp;
+    private static void swap(int[] arr) {
+        int length = arr.length;
+        int oneLess = length - 1;
+        for(int j=0; j<length-1; j++) { 
+            for(int k=0; k<oneLess-j; k++) { 
+                if(arr[k] > arr[k+1]) { 
+                    int temp = arr[k]; 
+                    arr[k] = arr[k+1];
+                    arr[k+1] = temp;
                 }
             }
+        print(arr);
         }
     }
-}
 
+    private static void print(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println("\n");
+    }
+}
